d30692404b562b6d9c0b95a367ce4752e79782a3
Christopher Pace <myiahsade@gmail.com>
2018-12-12 11:23:39 -0600

    check in

---
+++ answer.py
@@ -0,0 +1,17 @@
+array = [[4, 5, 6, 1, 8], [2, 3, 4, 5], [7, 5, 3]]
+
+
+def ArrayAdder(array):
+    for i in array:
+        for t in array:
+         print(t)
+
+         x = (len(t))
+         p=""
+
+    print(x)
+
+    print(array)
+
+
+ArrayAdder([[4, 5, 6, 1, 8], [2, 3, 4, 5], [7, 5, 3]])
